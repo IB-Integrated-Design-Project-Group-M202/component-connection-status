aeab05d9e8adc9837e5d19f309ed74629e3d1770
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-11-15 07:48:37 +0000

    Update Left_and_Right_Wheels_Motor_Test.ino

--- Left_and_Right_Wheels_Motor_Test.ino
+++ Left_and_Right_Wheels_Motor_Test.ino
@@ -1,3 +1,5 @@
+/* Pragash Mohanarajah (C) November 2021. */
+
 /*
 This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
 It won't work with v1.x motor shields! Only for the v2's with built in PWM
