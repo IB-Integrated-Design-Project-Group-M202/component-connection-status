7547ffb944510fc82890cbc19c1a6ceb8de7485a
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-11-17 07:34:10 +0000

    Update Line_Sensor_Test.ino

--- Line_Sensor_Test.ino
+++ Line_Sensor_Test.ino
@@ -1,1 +1,15 @@
+// Define Electrical parameters
+const int leftLineSensor = 4; rightLineSensor = 7;
 
+void setup() {
+  // Configure Line Sensors as Inputs
+  pinMode(leftLineSensor, INPUT);
+  pinMode(rightLineSensor, INPUT);
+}
+
+void loop() {
+  // Continuously monitor the state of each line sensor.
+  // Tell Serial to turn Left and Right according to which sensor is high.
+  if (digitalRead(leftLineSensor) == HIGH) Serial.println(F("Turn Left"));
+  else if (digitalRead(leftLineSensor) == HIGH) Serial.println(F("Turn Right"));
+}
