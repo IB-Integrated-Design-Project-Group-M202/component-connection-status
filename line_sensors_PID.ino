b1b79faf661e4923bad90cc6c02aa548fc88c75f
CCCHHHRRR <caohr2002@gmail.com>
2021-11-29 11:41:25 +0000

    switched centre line sensor to digital 5

--- line_sensors_PID.ino
+++ line_sensors_PID.ino
@@ -6,19 +6,18 @@ bool accel = true, decel = false;
 uint8_t centreSpeed=0; //average speed of left and right wheel for updatting the motors
 
 //pin definition 
-const int lsc_pin=A2,  lsl_pin=A3, lsr_pin=A4;
+const int lsc_pin=5,  lsl_pin=A3, lsr_pin=A4;
 
 
 //sensor readouts
 int lsc=0, lsl=0, lsr=0;
-uint8_t lsc_mapped=0, lsl_mapped=0, lsr_mapped=0;
+uint8_t lsl_mapped=0, lsr_mapped=0;
 
 //floor and celling
-int lsc_max=980, lsl_max=380, lsr_max=920;
-int lsc_min=100, lsl_min=64, lsr_min=122;
+int lsl_max=380, lsr_max=920;
+int lsl_min=64, lsr_min=122;
 
 //thresholds
-const uint8_t lsc_threshold = 150;
 const uint8_t lsl_threshold = 150;
 const uint8_t lsr_threshold = 150;
 
@@ -27,9 +26,9 @@ bool on_line=false;
 bool junction=false;
 
 //PID parameters
-const float Kp=0;
-const float Ki=0;
-const float Kd=0;
+const float Kp=0.2;
+const float Ki=0.01;
+const float Kd=0.01;
 
 //PID variables
 int P=0, I=0, D=0, last_P=0;
@@ -40,12 +39,11 @@ uint8_t left_motor_speed, right_motor_speed;
 
 
 void update_linesensors(){
-    lsc = analogRead(lsc_pin);
+    lsc = digitalRead(lsc_pin);
     lsl = analogRead(lsl_pin);
     lsr = analogRead(lsr_pin);
-    lsc_mapped = map(lsc, lsc_min, lsc_max, 0, 255);
-    lsl_mapped = map(lsl, lsl_min, lsl_max, 0, 255);
-    lsr_mapped = map(lsr, lsr_min, lsr_max, 0, 255);
+    lsl_mapped = map(min(max(lsl, lsl_min), lsl_max), lsl_min, lsl_max, 0, 255);
+    lsr_mapped = map(min(max(lsr, lsr_min), lsr_max), lsr_min, lsr_max, 0, 255);
 }
 
 /*void callibrate(){
@@ -62,7 +60,7 @@ void reset_PID(){
 
 void if_on_line(){
     update_linesensors();
-    if(lsc_mapped>=lsc_threshold){
+    if(lsc==1){
         if(on_line==false){
             on_line=true;
             reset_PID();
@@ -101,11 +99,12 @@ void loop(){
     D=P-last_P;
     last_P=P;
     
-    speed_difference=Kp*P+Ki*I+Kd*D;
+    speed_difference=max(min((Kp*P+Ki*I+Kd*D), 45), -45);
 
-    left_motor_speed=centreSpeed - max(speed_difference, 45);
-    right_motor_speed=centreSpeed + max(speed_difference, 45);
+    left_motor_speed=centreSpeed - speed_difference;
+    right_motor_speed=centreSpeed + speed_difference;
 
-    //Serial.println(String(lsc)+"\t"+String(lsl)+"\t"+String(lsr));
-    Serial.println(String(lsc_mapped)+"\t"+String(lsl_mapped)+"\t"+String(lsr_mapped));
+    //Serial.println(String(lsc*1023)+"\t"+String(lsl)+"\t"+String(lsr));
+    //Serial.println(String(lsc*1023)+"\t"+String(lsl_mapped)+"\t"+String(lsr_mapped));
+    Serial.println(String(left_motor_speed)+"\t"+String(right_motor_speed)+"\t"+String(on_line));
 }
