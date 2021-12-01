4c6febe7a7f6e1a05b0c276a05cebb5a8c8aa937
CCCHHHRRR <caohr2002@gmail.com>
2021-12-01 13:25:50 +0000

    search and align confirmed (waiting to be integrated with identification)

--- Dummy_Align_PID.ino
+++ Dummy_Align_PID.ino
@@ -4,6 +4,8 @@
 
 bool accel = true, decel = false;
 
+
+//distance sensors
 #define echoPin 2
 #define trigPin 3
 double distance;
@@ -17,11 +19,13 @@ Adafruit_DCMotor *rightMotor = AFMS.getMotor(2); // Select and configure port M2
 uint8_t centreSpeed = 0, leftSpeed = 0, rightSpeed = 0, leftDirection = FORWARD, rightDirection = FORWARD;
 
 int speed_difference = 0;
-float gapf=0;
+
+int leftSpeedv, rightSpeedv;
+
 //mode
 bool in_range=false;
-bool locked=false;
-bool arrival=false;
+//bool locked=false;
+bool stop=false;
 bool sequence_timeout=false;
 //int in_range_indication=0;
 
@@ -29,21 +33,20 @@ unsigned long search_timer=0;
 unsigned long now_ms=0;
 
 
-int leftSpeedv, rightSpeedv;
-
-
-
 //parameters
 const float Kp=0.04;
 const float Ki=0.001;
 const float Kd=0.1;
-const int in_range_threashold=550;
-const uint8_t approachSpeed=150;
-const uint8_t turnSpeed=150;
-const unsigned long search_timeout=20000;
+const int in_range_threashold=750;
+const uint8_t approachSpeed=170;
+const uint8_t turnSpeed=170;
+const unsigned long search_timeout=10000;
 
 //PID variables
-long P=0, I=0, D=0, last_P=0;/*
+long P=0, I=0, D=0, last_P=0;
+float gapf=0;
+/*
+
 Naming conventions:
 
 S1 - Sensor 1
@@ -64,6 +67,8 @@ const int IRs1 = A0, IRs2 = A1, IRRd=7;
 //time parameters
 const unsigned long window_time=3000, hold_time=12500;
 const int a_size=20;
+
+
 int s1m1sa[a_size], s2m1sa[a_size];
 int a_i=0;
 int s1m1sat=0, s1m1saa=0, s2m1sat=0, s2m1saa=0;
@@ -85,12 +90,12 @@ double measure_distance_mm() {
     return distances[0];
 }
 
-void if_arrival(){
+void if_stop(){
 
     if(ranging_index%16==0){
         distance=measure_distance_mm();
         if(distance<=180 and distance!=-1){
-            arrival=true;
+            stop=true;
         }
     }
     ranging_index++;
@@ -146,6 +151,7 @@ void PID_update(){
         D=(P-last_P)/gapf;
         last_P=P;
         speed_difference=max(min((Kp*P+Ki*I+Kd*D), turnSpeed), -turnSpeed);
+        if_stop();
     }
     else{
         speed_difference=turnSpeed;
@@ -178,12 +184,12 @@ void motor_update(){
 }
 
 void setup() {
-    Serial.begin(9600);
+    //Serial.begin(9600);
     for (int i = 0; i < a_size; i++){
         s1m1sa[i]=0;
         s2m1sa[i]=0;
     }
-    Serial.println("0\t0\t0\t0\t-1023\t1023");
+    //Serial.println("0\t0\t0\t0\t-1023\t1023");
   if (!AFMS.begin()) { // Check whether the motor shield is properly connected
     while (1);
   }
@@ -194,11 +200,12 @@ void setup() {
 }
 
 
-void loop() {
+void search(){
+
     //Serial.println("loop"+String(sequence_timeout)+String(distance)+String(ranging_index));
     search_timer=millis();
     sequence_timeout=false;
-    while(arrival==false and sequence_timeout==false){
+    while(stop==false and sequence_timeout==false){
         IR_readout();
 
         IR_peak_update();
@@ -227,18 +234,18 @@ void loop() {
             ssum=s1m1saa+s2m1saa;
             sdiff=s1m1saa-s2m1saa;
             if_in_range();
-            if_arrival();
+
             PID_update();
             motor_update();
             if(((now_ms-search_timer)>=search_timeout & in_range==false)){
               sequence_timeout=true;
             }
-            Serial.println("search"+String(distance));
+            //Serial.println("search"+String(distance));
             //Serial.print(String(s1m1saa)+"\t"+String(s2m1saa)+'\t');
             //Serial.print(String(ssum)+"\t"+String(sdiff)+'\t');
             //Serial.println(String(P)+"\t"+String(I/1000)+"\t"+String(D)+'\t');
             //Serial.println(String(speed_difference)+"\t"+String(in_range*1024));
-            Serial.println(String(s1m1saa)+"\t"+String(s2m1saa)+'\t'+String(ssum)+"\t"+String(sdiff)+'\t'+String(P)+"\t"+String(I/1000)+"\t"+String(D)+'\t');
+           // Serial.println(String(s1m1saa)+"\t"+String(s2m1saa)+'\t'+String(ssum)+"\t"+String(sdiff)+'\t'+String(P)+"\t"+String(I/1000)+"\t"+String(D)+'\t');
 
             //Serial.println(String(s1m1saa)+'\t'+String(s2m1saa)+'\t'+String(in_range*1023)+'\t'+String(speed_difference));
         }
@@ -255,43 +262,61 @@ void loop() {
 
         now_ms=millis();
     }
+}
+
+void walk(){
     search_timer=millis();
     sequence_timeout=false;
-    while(distance>=200 & sequence_timeout==false){
-        distance=measure_distance_mm();
-        delay(10);
-        Serial.println("walk"+String(distance));
-        leftSpeedv=240;
-        rightSpeedv=200;
+    while(stop==false & sequence_timeout==false){
+        if_stop();
+        //Serial.println("walk"+String(distance));
+        leftSpeedv=245;
+        rightSpeedv=220;
         motor_update();
         now_ms=millis();
-        if(((now_ms-search_timer)>=2000)){
+        if(((now_ms-search_timer)>=3000)){
             sequence_timeout=true;
         }
 
     }
     
     
+}
 
-    if_arrival();
-    if(arrival==true){
+void escape(){
+    stop=false;
+    leftSpeedv=-250;
+    rightSpeedv=150;
+    leftSpeed=abs(leftSpeedv);
+    rightSpeed=abs(rightSpeedv);
+    motor_update();
+    delay(2000);
+}
+
+void search_and_align_and_identify(){
+
+    search();
+    walk();
+
+    if_stop();
+    if(stop==true){
         leftSpeed=0;
         rightSpeed=0;
         motor_update();
     }
     
     if_in_range();
+
     if(in_range=true){
-        delay(5000);
+        delay(1000);
         //put identification here and delete delay
     }
 
-    arrival=false;
-    leftSpeedv=-200;
-    rightSpeedv=150;
-    leftSpeed=abs(leftSpeedv);
-    rightSpeed=abs(rightSpeedv);
-    motor_update();
-    delay(3000);
+    escape();
 
-} 
+}
+
+
+void loop() {
+    search_and_align_and_identify();
+}  
