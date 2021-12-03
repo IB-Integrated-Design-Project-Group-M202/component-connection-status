c09f490df51bbe944c7d9865f976072b4ef8b28e
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-12-03 15:12:25 +0000

    Update Test_Infrared_Sensors_Smoothed_Maximum_Update.ino

--- Test_Infrared_Sensors_Smoothed_Maximum_Update.ino
+++ Test_Infrared_Sensors_Smoothed_Maximum_Update.ino
@@ -40,36 +40,31 @@ void loop() {
     s1 = analogRead(IRs1);
     now = micros();
 
-    //hold phase
-    if(s1m1d==false){
-        s1m1tm2=now-s1m1t2;
-        //timeout
-        if(s1m1tm2>=hold_time){
-            //initiates detection window
-            s1m1d=true;
-            s1m1t1=now;
-        }
-    }
-
-    //detection phase
-    if(s1m1d==true){
-        //maximum value update
-        if(s1 > s1m1){
+    if(s1 > s1m1){
             s1m1 = s1;
             s1m1t1 = now;
-        }
-        //s1m1 = s1m1-1'
-        s1m1tm1=now-s1m1t1;
+            if(s1m1>s1m1s){
+                s1m1s=s1m1;
+                s1m1t2=now;
+            }
+    }
+    s1m1tm1=now-s1m1t1;
 
+    //timeout
+    if(s1m1tm1>=window_time){
+        //initialises hold
+        s1m1d=false;
+        s1m1=0;
+        Serial.println(s1m1s);
+    }
+
+    s1m1tm2=now-s1m1t2;
         //timeout
-        if(s1m1tm1>=window_time){
-            //initialises hold
-            s1m1d=false;
-            s1m1t2=now;
-            s1m1s=s1m1;
-            s1m1=0;
-        }
-
+    if(s1m1tm2>=hold_time){
+            //initiates detection window
+        s1m1d=true;
+        s1m1t2=now;
+        s1m1s=s1m1;
     }
 
     gap=now-lastt;
